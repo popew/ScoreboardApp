4d6e999dbb1cd125212c3fdebbd5b586b8d0779c
Maggus <wojtazy503@gmail.com>
2022-09-13 10:10:37 +0200

    Add infrastructure project

---
+++ src/Infrastructure/ScoreboardApp.Infrastructure/Persistence/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ScoreboardApp.Infrastructure.Persistence
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
