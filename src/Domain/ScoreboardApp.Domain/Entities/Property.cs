eb3db31ef28178c707fe0b6339479c6cfee3df94
Maggus <wojtazy503@gmail.com>
2022-09-14 00:02:21 +0200

    Add Domain class library

---
+++ src/Domain/ScoreboardApp.Domain/Entities/Property.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreboardApp.Domain.Entities
+{
+    internal class Property
+    {
+    }
+}
