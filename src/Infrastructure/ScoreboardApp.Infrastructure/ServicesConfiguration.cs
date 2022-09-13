00fec1824e667a97bb2400ae2fd43dd4120e111e
Wojciech Pope <36055845+Maggus503@users.noreply.github.com>
2022-09-13 10:14:56 +0200

    Add infrastructure project (#7)
    Co-authored-by: Maggus <wojtazy503@gmail.com>

---
+++ src/Infrastructure/ScoreboardApp.Infrastructure/ServicesConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ScoreboardApp.Infrastructure
+{
+    public static class ServicesConfiguration
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+    }
+}
