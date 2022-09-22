c0856aadb0ac7d1ab7f70b04354cf6274e70a572
Maggus503 <popewojciech@gmail.com>
2022-09-22 22:24:02 +0200

    Add priority enums

---
+++ src/Domain/ScoreboardApp.Domain/Enums/Priority.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Tracing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreboardApp.Domain.Enums
+{
+    public enum Priority
+    {
+        None = 0,
+        Low = 1,
+        Medium =2,
+        High = 3
+    }
+}
