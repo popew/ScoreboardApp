a4fdc363713e68050f70cfb99f5d3b9925e8b124
Maggus503 <popewojciech@gmail.com>
2022-09-22 22:27:38 +0200

    Update habit tracker

--- src/Domain/ScoreboardApp.Domain/Entities/HabitTracker.cs
+++ src/Domain/ScoreboardApp.Domain/Entities/HabitTracker.cs
@@ -1,11 +1,12 @@
 ﻿using ScoreboardApp.Domain.Entities.Commons;
+using ScoreboardApp.Domain.Enums;
 
 namespace ScoreboardApp.Domain.Entities
 {
     public class HabitTracker : BaseEntity
     {
         public string Title { get; set; } = default!;
-        public uint? Order { get; set; }
+        public Priority Priority { get; set; } = Priority.None;
 
         public IList<Habit> Habits { get; private set; } = new List<Habit>();
     }
