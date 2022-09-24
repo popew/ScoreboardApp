1f66cacf77841b4880f57ef5d9923bf0cffee5b7
Maggus503 <popewojciech@gmail.com>
2022-09-25 01:22:53 +0200

    Add required packages

---
+++ src/Application/ScoreboardApp.Application/ServicesConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreboardApp.Application
+{
+    public static class ServicesConfiguration
+    {
+        public static IServiceCollection AddApplicationservices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            return services;
+        }
+    }
+}
