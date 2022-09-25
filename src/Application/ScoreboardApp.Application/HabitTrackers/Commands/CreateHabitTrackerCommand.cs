a743340dd71862b2b22ba073247b7cdbdf6cacb8
Maggus503 <popewojciech@gmail.com>
2022-09-25 22:13:15 +0200

    Add CreateHabitTrackerCommand

---
+++ src/Application/ScoreboardApp.Application/HabitTrackers/Commands/CreateHabitTrackerCommand.cs
@@ -0,0 +1,39 @@
+﻿using MediatR;
+using ScoreboardApp.Domain.Entities;
+using ScoreboardApp.Infrastructure.Persistence;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreboardApp.Application.HabitTrackers.Commands
+{
+    public class CreateHabitTrackerCommand : IRequest<Guid>
+    {
+        public string Title { get; set; } = default!;
+    }
+
+    public class CreateHabitTrackerCommandHanlder : IRequestHandler<CreateHabitTrackerCommand, Guid>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateHabitTrackerCommandHanlder(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<Guid> Handle(CreateHabitTrackerCommand request, CancellationToken cancellationToken)
+        {
+            var habitTrackerEntity = new HabitTracker()
+            {
+                Title = request.Title
+            };
+
+            _context.HabitTrackers.Add(habitTrackerEntity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return habitTrackerEntity.Id;
+        }
+    }
+}
