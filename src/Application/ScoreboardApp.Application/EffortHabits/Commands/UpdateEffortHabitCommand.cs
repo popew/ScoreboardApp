528a6604c7acb92643f0a15a3fe21b17ed7d00b8
Maggus503 <popewojciech@gmail.com>
2022-10-02 14:18:23 +0200

    Add UpdateEffortHabitCommand

--- src/Application/ScoreboardApp.Application/EffortHabits/Commands/UpdateEffortHabitCommand.cs
+++ src/Application/ScoreboardApp.Application/EffortHabits/Commands/UpdateEffortHabitCommand.cs
@@ -1,6 +1,47 @@
-﻿namespace ScoreboardApp.Application.Habits.Commands
+﻿using MediatR;
+using ScoreboardApp.Application.Commons.Enums;
+using ScoreboardApp.Infrastructure.Persistence;
+
+namespace ScoreboardApp.Application.Habits.Commands
 {
-    internal class UpdateEffortHabitCommand
+    public sealed record UpdateEffortHabitCommand : IRequest<UpdateEffortHabitCommandResponse>
     {
+        public Guid Id { get; init; }
+        public string Title { get; init; } = default!;
+        public string Description { get; init; } = default!;
+
+        public string? Unit { get; init; } = default!;
+        public double? Goal { get; init; }
+
+        public EffortHabitSubtypeMapping Subtype { get; init; }
+        public Guid HabitTrackerId { get; init; }
+    }
+
+    public sealed record UpdateEffortHabitCommandResponse
+    {
+        public Guid Id { get; init; }
+        public string Title { get; init; } = default!;
+        public string Description { get; init; } = default!;
+
+        public string? Unit { get; init; } = default!;
+        public double? Goal { get; init; }
+
+        public EffortHabitSubtypeMapping Subtype { get; init; }
+        public Guid HabitTrackerId { get; init; }
+    }
+
+    public sealed class UpdateEfforHabitCommandHandler : IRequestHandler<UpdateEffortHabitCommand, UpdateEffortHabitCommandResponse>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public UpdateEfforHabitCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<UpdateEffortHabitCommandResponse> Handle(UpdateEffortHabitCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
