0b89ed0b6c75ad8cac6476a46c7f943824da0460
Wojciech Pope <popewojciech@gmail.com>
2022-10-15 14:09:36 +0200

    Add opentelemetry configuration (#18)

--- src/Api/ScoreboardApp.Api/ServicesConfiguration.cs
+++ src/Api/ScoreboardApp.Api/ServicesConfiguration.cs
@@ -3,6 +3,10 @@ using ScoreboardApp.Api.Filters;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Versioning;
+using OpenTelemetry;
+using OpenTelemetry.Trace;
+using OpenTelemetry.Extensions.Hosting;
+using OpenTelemetry.Resources;
 
 namespace ScoreboardApp.Api
 {
@@ -21,8 +25,23 @@ namespace ScoreboardApp.Api
             {
                 options.AssumeDefaultVersionWhenUnspecified = true;
                 options.DefaultApiVersion = ApiVersion.Default;
-                options.ApiVersionReader = new HeaderApiVersionReader("api-version");
+                options.ApiVersionReader = new HeaderApiVersionReader("X-api-version");
                 options.ReportApiVersions = true;
+            });
+
+            services.AddOpenTelemetryTracing(builder =>
+            {
+                builder
+                    .SetResourceBuilder(ResourceBuilder
+                    .CreateDefault()
+                    .AddService("ScoreboardApp"))
+                    .AddZipkinExporter(options =>
+                    {
+                        options.Endpoint = new Uri("http://localhost:9411/api/v2/spans"); // Hardcoding it for now, because the library throws for some reason when the value is read from appsettings
+                    })
+                    .AddHttpClientInstrumentation()
+                    .AddAspNetCoreInstrumentation()
+                    .AddSqlClientInstrumentation();
             });
 
             return services;
