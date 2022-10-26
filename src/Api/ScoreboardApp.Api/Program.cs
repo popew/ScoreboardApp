16d5fb33b5d45b918d2cfac8bca7cd47e4cb9a76
Maggus503 <popewojciech@gmail.com>
2022-10-26 14:47:05 +0200

    Update swagger configuration with authorization info

--- src/Api/ScoreboardApp.Api/Program.cs
+++ src/Api/ScoreboardApp.Api/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 using ScoreboardApp.Api;
 using ScoreboardApp.Application;
 using ScoreboardApp.Infrastructure;
@@ -12,7 +13,35 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => c.UseDateOnlyTimeOnlyStringConverters());
+builder.Services.AddSwaggerGen(c =>
+{
+    c.UseDateOnlyTimeOnlyStringConverters();
+
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Scheme = "bearer",
+        Description = "Please insert JWT token into field"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+
+});
 
 builder.Services.AddApplicationservices(builder.Configuration);
 builder.Services.AddInfrastructureServices(builder.Configuration);
