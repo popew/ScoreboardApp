4606c0a8ae36b67f1fb26a205ec878425c2a61d7
Maggus503 <popewojciech@gmail.com>
2022-11-04 02:08:15 +0100

    Add integration tests initial project

---
+++ test/Api/ScoreboardApp.Api.IntegrationTests/ScoreboardAppApiFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoreboardApp.Api.IntegrationTests
+{
+    public class ScoreboardAppApiFactory : WebApplicationFactory<IApiMarker>, IAsyncLifetime
+    {
+        public Task InitializeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        Task IAsyncLifetime.DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
