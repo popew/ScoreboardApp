3216ee5b7205fa0db5eea4c47900a11a0ecc2102
Maggus503 <popewojciech@gmail.com>
2022-11-13 19:26:47 +0100

    Add current user service from clean architecture template

---
+++ src/Api/ScoreboardApp.Api/Services/CurrentUserService.cs
@@ -0,0 +1,17 @@
+﻿using ScoreboardApp.Application.Commons.Interfaces;
+using System.Security.Claims;
+
+namespace ScoreboardApp.Api.Services
+{
+    public class CurrentUserService : ICurrentUserService
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+
+        public CurrentUserService(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+        }
+
+        public string? UserId => _httpContextAccessor.HttpContext?.User?.FindFirstValue(ClaimTypes.NameIdentifier);
+    }
+}
