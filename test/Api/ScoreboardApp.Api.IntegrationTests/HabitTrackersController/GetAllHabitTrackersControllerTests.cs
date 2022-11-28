b92b9a9cfd157356561385385b881719762992da
Maggus503 <popewojciech@gmail.com>
2022-11-28 03:34:13 +0100

    Add get all tests

--- test/Api/ScoreboardApp.Api.IntegrationTests/HabitTrackersController/GetAllHabitTrackersControllerTests.cs
+++ test/Api/ScoreboardApp.Api.IntegrationTests/HabitTrackersController/GetAllHabitTrackersControllerTests.cs
@@ -1,6 +1,56 @@
-﻿namespace ScoreboardApp.Api.IntegrationTests.HabitTrackersController
+﻿using ScoreboardApp.Application.DTOs;
+using ScoreboardApp.Application.DTOs.Enums;
+using ScoreboardApp.Application.HabitTrackers.Commands;
+using ScoreboardApp.Application.HabitTrackers.Queries;
+
+namespace ScoreboardApp.Api.IntegrationTests.HabitTrackersController
 {
-    internal class GetAllHabitTrackersControllerTests
+    public class GetAllHabitTrackersControllerTests : IClassFixture<ScoreboardAppApiFactory>
     {
+        private const string Endpoint = "api/HabitTrackers";
+        private readonly HttpClient _apiClient;
+        private readonly ScoreboardAppApiFactory _apiFactory;
+
+        private readonly Faker<CreateHabitTrackerCommand> _createCommandGenerator = new Faker<CreateHabitTrackerCommand>()
+            .RuleFor(x => x.Title, faker => faker.Random.String2(1, 200))
+            .RuleFor(x => x.Priority, faker => PriorityMapping.NotSet);
+
+        public GetAllHabitTrackersControllerTests(ScoreboardAppApiFactory apiFactory)
+        {
+            _apiFactory = apiFactory;
+            _apiClient = apiFactory.CreateClient();
+
+            _apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiFactory.TestUser1.Token);
+        }
+
+        [Fact]
+        public async Task GetAll_ReturnsListOfHabitTrackers_WhenHabitTrackersExist()
+        {
+            // Arrange
+            var createdObject = await CreateHabitTracker();
+
+            // Act
+            var getHttpResponse = await _apiClient.GetAsync($"{Endpoint}");
+
+            // Assert
+            getHttpResponse.Should().HaveStatusCode(HttpStatusCode.OK);
+
+            var receivedObject = await getHttpResponse.Content.ReadFromJsonAsync<List<HabitTrackerDTO>>();
+
+            receivedObject.Should().NotBeNull();
+            receivedObject.Should().ContainEquivalentOf(createdObject);
+        }
+
+        private async Task<CreateHabitTrackerCommandResponse?> CreateHabitTracker()
+        {
+            var habitTracker = _createCommandGenerator.Generate();
+            var createHttpResponse = await _apiClient.PostAsJsonAsync(Endpoint, habitTracker);
+
+            createHttpResponse.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            return await createHttpResponse.Content.ReadFromJsonAsync<CreateHabitTrackerCommandResponse>();
+        }
+
     }
+
 }
