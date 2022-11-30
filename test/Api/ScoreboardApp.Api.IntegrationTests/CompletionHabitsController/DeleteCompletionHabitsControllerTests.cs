98cfb9ff9e5ca991cde4188f88a018313fcd4e91
Maggus503 <popewojciech@gmail.com>
2022-11-30 03:18:14 +0100

    Add initial DeleteCompletionHabits test

--- test/Api/ScoreboardApp.Api.IntegrationTests/CompletionHabitsController/DeleteCompletionHabitsControllerTests.cs
+++ test/Api/ScoreboardApp.Api.IntegrationTests/CompletionHabitsController/DeleteCompletionHabitsControllerTests.cs
@@ -1,12 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ScoreboardApp.Application.DTOs.Enums;
+using ScoreboardApp.Application.Habits.Commands;
+using ScoreboardApp.Application.HabitTrackers.Commands;
 
 namespace ScoreboardApp.Api.IntegrationTests.CompletionHabitsController
 {
-    internal class DeleteCompletionHabitsControllerTests
+    public class DeleteCompletionHabitsControllerTests : IClassFixture<ScoreboardAppApiFactory>
     {
+        private const string Endpoint = TestHelpers.Endpoints.CompletionHabits;
+        private readonly HttpClient _apiClient;
+        private readonly ScoreboardAppApiFactory _apiFactory;
+
+        private readonly Faker<CreateHabitTrackerCommand> _createTrackerCommandGenerator = new Faker<CreateHabitTrackerCommand>()
+            .RuleFor(x => x.Title, faker => faker.Random.String2(1, 200))
+            .RuleFor(x => x.Priority, faker => PriorityMapping.NotSet);
+
+        private readonly Faker<CreateCompletionHabitCommand> _createCompletionHabitCommandGenerator = new Faker<CreateCompletionHabitCommand>()
+            .RuleFor(x => x.Description, faker => faker.Random.String2(1, 400))
+            .RuleFor(x => x.Title, faker => faker.Random.String2(1, 200));
+
+        public DeleteCompletionHabitsControllerTests(ScoreboardAppApiFactory apiFactory)
+        {
+            _apiFactory = apiFactory;
+            _apiClient = apiFactory.CreateClient();
+
+            _apiClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _apiFactory.TestUser1.Token);
+        }
+
+        [Fact]
+        public async Task Delete_DeletesCompletionHabit_WhenHabitExists()
+        {
+            // Arrange
+            var habitTracker = await TestHelpers.CreateHabitTracker(_apiClient, _createTrackerCommandGenerator);
+
+            var completionHabitGenerator = _createCompletionHabitCommandGenerator.Clone()
+                .RuleFor(x => x.HabitTrackerId, faker => habitTracker!.Id);
+
+            var completionHabit = await TestHelpers.CreateCompletionHabit(_apiClient, completionHabitGenerator);
+
+            // Act
+            var deleteCompletionHabitResponse = await _apiClient.DeleteAsync($"{Endpoint}/{completionHabit!.Id}");
+
+            // Assert
+            deleteCompletionHabitResponse.Should().HaveStatusCode(HttpStatusCode.NoContent);
+        }
     }
 }
